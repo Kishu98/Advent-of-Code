dcd711593c737e0de9eefb65888b0755cbd36d68
Kishu98 <kishujain.1998@gmail.com>
2024-10-29 19:11:48 +0530

    day-5

--- 2023/Day-5/main.go
+++ 2023/Day-5/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -14,26 +15,56 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// fmt.Println(string(file))
+	var totalSeeds []int
 
 	lines := strings.Split(string(file), "\n\n")
-
 	seeds := strings.Split(lines[0], " ")
-	seeds = append(seeds[:0], seeds[1:]...)
-	lines = append(lines[:0], lines[1:]...)
-	// fmt.Println(lines[0])
-
-	for _, seed := range seeds {
-		fmt.Println(seed)
-		for _, line := range lines {
-			mapNums := strings.Split(line, "\n")
-			mapNums = append(mapNums[:0], mapNums[1:]...)
-			// fmt.Println(mapNums, ", ")
-            for _, mapNum := range(mapNums) {
-                nums := strings.Split(mapNum, " ")
-                fmt.Println(nums, ",")
-            }
+	seeds = seeds[1:]
+	for i := 0; i < len(seeds); i = i + 2 {
+		start, _ := strconv.Atoi(seeds[i])
+        // fmt.Println("Start -->", start)
+		end, _ := strconv.Atoi(seeds[i+1])
+        end = start + end
+        // fmt.Println("End -->", end)
+		for j := start; j < end; j++ {
+			totalSeeds = append(totalSeeds, j)
 		}
 	}
+	blocks := lines[1:]
+	// fmt.Println(totalSeeds)
+	// seed := 79
+	minLocation := 9999999999999
+	for _, seed := range totalSeeds {
 
+		for _, block := range blocks {
+			// fmt.Println(block)
+			// fmt.Println("----------------------------")
+			blk := strings.Split(block, "\n")
+			for i := 1; i < len(blk); i++ {
+				if blk[i] == "" {
+					continue
+				}
+				// fmt.Println("-->", blk[i])
+				nums := strings.Split(blk[i], " ")
+				dest, _ := strconv.Atoi(nums[1])
+				source, _ := strconv.Atoi(nums[0])
+				ran, _ := strconv.Atoi(nums[2])
+				if seed >= dest && seed < (dest+ran) {
+					// fmt.Println(seed, "+", dest, "-", source)
+					seed = seed + source - dest
+					break
+				}
+				// for j := 0; j < 3; j++ {
+				// 	nums := strings.Split(blk[i], " ")
+				// 	fmt.Println("\t-->", nums[j])
+				// }
+			}
+			// fmt.Println(seed)
+		}
+		if minLocation > seed {
+			minLocation = seed
+		}
+		// fmt.Println(seed)
+	}
+	fmt.Println(minLocation)
 }
