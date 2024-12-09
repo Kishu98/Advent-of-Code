629566c120394dfe3e476b1adfc43d424d015a16
Kishu98 <kishujain.1998@gmail.com>
2024-12-09 09:54:04 +0530

    day6

--- 2024/Day-6/main.go
+++ 2024/Day-6/main.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 )
+
+type Position struct {
+	row, col int
+}
 
 func main() {
 	file, err := os.OpenFile("Input.txt", os.O_CREATE|os.O_RDONLY, 0644)
@@ -16,131 +19,99 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	var grid [][]string
+	var grid [][]rune
 
 	for scanner.Scan() {
-		grid = append(grid, strings.Split(scanner.Text(), ""))
+		grid = append(grid, []rune(scanner.Text()))
 	}
 
-	currPos := getPosition(grid)
-
-	going = "up"
-
-	for checkPos(grid, getPosition(grid), &going) {
-		fmt.Println("*********************************************")
-		printGrid(grid)
-		fmt.Println("*********************************************")
+	directions := map[rune]Position{
+		'^': {-1, 0},
+		'>': {0, 1},
+		'v': {1, 0},
+		'<': {0, -1},
 	}
 
-	fmt.Println("*********************************************")
-	printGrid(grid)
-	fmt.Println("*********************************************")
+	turnRight := map[rune]rune{
+		'^': '>',
+		'>': 'v',
+		'v': '<',
+		'<': '^',
+	}
 
-    allPos := getAllPos(grid)
+	simulate := func(grid [][]rune, startPos Position, startDir rune) (map[Position]bool, bool) {
+		currentPos := startPos
+		currentDir := startDir
+		visitedStates := make(map[struct {
+			pos Position
+			dir rune
+		}]bool)
+		visitedPositions := make(map[Position]bool)
+		rows, cols := len(grid), len(grid[0])
 
-	fmt.Println(len(allPos))
+		for {
+			state := struct {
+				pos Position
+				dir rune
+			}{currentPos, currentDir}
+			if visitedStates[state] {
+				return visitedPositions, true
+			}
+			visitedStates[state] = true
+			visitedPositions[currentPos] = true
 
-    grid[currPos[0]][currPos[1]] = "^"
-    going = "up"
-    printGrid(grid)
-}
+			dr, dc := directions[currentDir].row, directions[currentDir].col
+			nextPos := Position{currentPos.row + dr, currentPos.col + dc}
 
-func getAllPos(grid [][]string) [][]int {
-	count := [][]int{}
+			if nextPos.row < 0 || nextPos.row >= rows || nextPos.col < 0 || nextPos.col >= cols {
+				return visitedPositions, false
+			}
 
-	for i, row := range grid {
-		for j, col := range row {
-			if col == "X" {
-                grid[i][j] = "."
-                count = append(count, []int{i, j})
+			if grid[nextPos.row][nextPos.col] == '#' {
+				currentDir = turnRight[currentDir]
+			} else {
+				currentPos = nextPos
 			}
 		}
 	}
 
-	return count
+	var startPos Position
+	var startDir rune
+	for r, row := range grid {
+		for c, cell := range row {
+			if _, ok := directions[cell]; ok {
+				startPos = Position{r, c}
+				startDir = cell
+				break
+			}
+		}
+	}
+
+	originalVisited, _ := simulate(grid, startPos, startDir)
+
+	loopPositions := make(map[Position]bool)
+	for r, row := range grid {
+		for c, cell := range row {
+			pos := Position{r, c}
+			if _, visited := originalVisited[pos]; visited && cell == '.' {
+				grid[r][c] = '#'
+				_, isLoop := simulate(grid, startPos, startDir)
+				if isLoop {
+					printGrid(grid)
+					loopPositions[pos] = true
+				}
+				grid[r][c] = '.'
+			}
+		}
+	}
+
+	fmt.Println(len(loopPositions))
 }
 
-func printGrid(grid [][]string) {
+func printGrid(grid [][]rune) {
+	fmt.Println("*********************************************")
 	for _, row := range grid {
-		fmt.Println(row)
+		fmt.Println(string(row))
 	}
-	fmt.Println("Going -->", going)
 }
 
-var going string
-var area int
-
-func checkPos(grid [][]string, pos []int, going *string) bool {
-	row, col := &pos[0], &pos[1]
-	switch *going {
-	case "up":
-		if *row-1 < 0 {
-			grid[*row][*col] = "X"
-			return false
-		}
-		if grid[*row-1][*col] == "#" {
-			*going = "right"
-		} else {
-			grid[*row][*col] = "X"
-			area++
-			*row = *row - 1
-			grid[*row][*col] = "^"
-		}
-	case "right":
-		if *col+1 > len(grid[*row])-1 {
-			grid[*row][*col] = "X"
-			return false
-		}
-		if grid[*row][*col+1] == "#" {
-			*going = "down"
-		} else {
-			area++
-			grid[*row][*col] = "X"
-			*col = *col + 1
-			grid[*row][*col] = "^"
-		}
-	case "down":
-		if *row+1 > len(grid)-1 {
-			grid[*row][*col] = "X"
-			return false
-		}
-		if grid[*row+1][*col] == "#" {
-			*going = "left"
-		} else {
-			grid[*row][*col] = "X"
-			area++
-			*row = *row + 1
-			grid[*row][*col] = "^"
-		}
-	case "left":
-		if *col-1 < 0 {
-			grid[*row][*col] = "X"
-			return false
-		}
-		if grid[*row][*col-1] == "#" {
-			*going = "up"
-		} else {
-			grid[*row][*col] = "X"
-			area++
-			*col = *col - 1
-			grid[*row][*col] = "^"
-		}
-	}
-
-	// if *row > len(grid)-1 || *row < 0 || *col > len(grid[*row])-1 || *col < 0 {
-	// 	return false
-	// }
-
-	return true
-}
-
-func getPosition(grid [][]string) []int {
-	for i, row := range grid {
-		for j, col := range row {
-			if col == "^" {
-				return []int{i, j}
-			}
-		}
-	}
-	return []int{0, 0}
-}
