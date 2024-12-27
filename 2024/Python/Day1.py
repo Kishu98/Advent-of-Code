ab5302df00dd5db43bd5ca210f49c02ddb50b867
Kishu98 <kishujain.1998@gmail.com>
2024-12-28 01:44:23 +0530

    python-day1,2,3

---
+++ 2024/Python/Day1.py
@@ -0,0 +1,17 @@
+a = list(
+    map(
+        list,
+        zip(
+            *[
+                list(map(int, line.split()))
+                for line in open("Input.txt").read().splitlines()
+            ]
+        ),
+    )
+)
+for k in a:
+    k.sort()
+
+print(sum(abs(x - y) for x, y in zip(*a)))
+
+# print(sum(x * r.count(x) for x in l1))
