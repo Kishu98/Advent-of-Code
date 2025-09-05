37d77584ff61ac802c1bf4bb7aad84c74626e045
Kishu <kishujain.1998@gmail.com>
2025-09-06 01:44:05 +0530

    refactor: 2024/Day6

--- 2024/06/part1.go
+++ 2024/06/part1.go
@@ -1,81 +1,83 @@
 package main
 
 import (
-	"slices"
 	"strings"
 
 	"github.com/Kishu98/AdventOfCode/helpers"
+)
+
+var (
+	Directions = map[rune]Point{
+		'^': {-1, 0},
+		'>': {0, 1},
+		'v': {1, 0},
+		'<': {0, -1},
+	}
+
+	facingOrder = "^>v<"
 )
 
 type Point struct {
 	x, y int
 }
 
-var Dirs = map[rune]Point{
-	'^': {-1, 0},
-	'>': {0, 1},
-	'v': {1, 0},
-	'<': {0, -1},
+type Guard struct {
+	pos    Point
+	facing rune
 }
-
-var facingOrder = []rune{'^', '>', 'v', '<'}
 
 func part1(filename string) int {
 	grid := helpers.ParseGrid(filename, func(r rune) rune { return r })
-	gridMap := buildGridMap(grid)
-	guardPos, guardFacing := findGuard(grid)
+	guard := findGuard(grid)
 
-	return len(moveGuard(grid, gridMap, guardPos, guardFacing))
+	guardPath := tracePath(grid, guard)
+	return len(guardPath)
 }
 
-func moveGuard(grid [][]rune, gridMap map[Point]rune, pos Point, facing rune) map[Point]bool {
+func tracePath(grid [][]rune, guard Guard) map[Point]bool {
 	visited := make(map[Point]bool)
-	visited[pos] = true
-
-	for {
-		dir := Dirs[facing]
-		next := Point{pos.x + dir.x, pos.y + dir.y}
-		if !inBound(next, grid) {
-			break
-		}
-		if gridMap[next] != '#' {
-			pos = next
-			visited[pos] = true
-		} else {
-			facing = turnRight(facing)
-		}
+	visited[guard.pos] = true
+	for guard.nextMove(grid) {
+		visited[guard.pos] = true
 	}
 
 	return visited
 }
 
-func inBound(guardPos Point, grid [][]rune) bool {
-	return (guardPos.x >= 0 && guardPos.y >= 0 && guardPos.x < len(grid) && guardPos.y < len(grid[0]))
+func (g *Guard) turnRight() {
+	currentFacing := strings.IndexRune(facingOrder, g.facing)
+	g.facing = rune(facingOrder[(currentFacing+1)%4])
 }
 
-func turnRight(f rune) rune {
-	return facingOrder[(slices.Index(facingOrder, f)+1)%len(facingOrder)]
+func (g *Guard) nextMove(grid [][]rune) bool {
+	dir := Directions[g.facing]
+	dx, dy := g.pos.x+dir.x, g.pos.y+dir.y
+	next := Point{dx, dy}
+	if !inBounds(grid, next) {
+		return false
+	}
+	if grid[next.x][next.y] != '#' {
+		g.pos = next
+	} else {
+		g.turnRight()
+	}
+	return true
 }
 
-func findGuard(grid [][]rune) (Point, rune) {
+func inBounds(grid [][]rune, p Point) bool {
+	return p.x >= 0 && p.y >= 0 && p.x < len(grid) && p.y < len(grid[0])
+}
+
+func findGuard(grid [][]rune) Guard {
+	var guard Guard
 	for x, row := range grid {
 		for y, col := range row {
-			if strings.ContainsRune("V<>^", col) {
-				return Point{x, y}, col
+			if strings.ContainsRune(facingOrder, col) {
+				guard.pos = Point{x, y}
+				guard.facing = col
 			}
 		}
 	}
 
-	return Point{}, '^'
-}
-
-func buildGridMap(grid [][]rune) map[Point]rune {
-	gridMap := make(map[Point]rune)
-	for x, row := range grid {
-		for y, col := range row {
-			gridMap[Point{x, y}] = col
-		}
-	}
-
-	return gridMap
+	return guard
 }
--- 2024/06/part2.go
+++ 2024/06/part2.go
@@ -1,53 +1,35 @@
 package main
 
 import (
-	"slices"
-
 	"github.com/Kishu98/AdventOfCode/helpers"
 )
 
 func part2(filename string) int {
 	grid := helpers.ParseGrid(filename, func(r rune) rune { return r })
-	gridMap := buildGridMap(grid)
-	startPos, startingFacing := findGuard(grid)
+	guard := findGuard(grid)
+	guardPath := tracePath(grid, guard)
 
 	result := 0
-	visited := moveGuard(grid, gridMap, startPos, startingFacing)
-	for pos := range visited {
-		if pos == startPos {
-			continue
-		}
-		gridMap[pos] = '#'
-		if inCycle(grid, gridMap, startPos, startingFacing) {
+	for path := range guardPath {
+		grid[path.x][path.y] = '#'
+		if checkCycle(grid, guard) {
 			result++
 		}
-		gridMap[pos] = '.'
+		grid[path.x][path.y] = '.'
 	}
 
 	return result
 }
 
-func inCycle(grid [][]rune, gridMap map[Point]rune, pos Point, facing rune) bool {
-	seen := make(map[[3]int]bool)
-
-	for {
-		dir := Dirs[facing]
-		next := Point{pos.x + dir.x, pos.y + dir.y}
-
-		if !inBound(next, grid) {
-			return false
-		}
-
-		state := [3]int{pos.x, pos.y, slices.Index(facingOrder, facing)}
+func checkCycle(grid [][]rune, guard Guard) bool {
+	seen := make(map[Guard]bool)
+	for guard.nextMove(grid) {
+		state := Guard{guard.pos, guard.facing}
 		if seen[state] {
 			return true
 		}
 		seen[state] = true
-
-		if gridMap[next] != '#' {
-			pos = next
-		} else {
-			facing = turnRight(facing)
-		}
 	}
+
+	return false
 }
